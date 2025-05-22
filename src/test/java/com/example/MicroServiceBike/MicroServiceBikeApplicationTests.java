c6f921ad75dc8c9a853f61d9881f23edc0859c47
JesusCiber <fjesuscl@gmail.com>
2025-05-22 16:25:53 +0200

    init commit

---
+++ src/test/java/com/example/MicroServiceBike/MicroServiceBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
