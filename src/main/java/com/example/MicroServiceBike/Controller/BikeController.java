8daf87aeb1e8a7baeec85f14a040f25e7b4ed27a
JesusCiber <fjesuscl@gmail.com>
2025-05-22 18:10:40 +0200

    Controller

---
+++ src/main/java/com/example/MicroServiceBike/Controller/BikeController.java
@@ -0,0 +1,11 @@
+package com.example.MicroServiceBike.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bike")
+public class BikeController {
+
+}
